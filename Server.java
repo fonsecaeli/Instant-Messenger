fde066b83b71d1d7d49cd12ad8e9ce69bf8cbd0d
Eli <eli.fonseca@live.com>
2016-02-13 18:10:47 -0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -9,146 +9,201 @@ import javax.swing.*;
 
 public class Server extends JFrame {
 
-	private JTextField userText;
-	private JTextArea chatWindow;
-	private ObjectOutputStream output;
-	private ObjectInputStream input;
-	private ServerSocket server;
-	private Socket connection;
-	private final int HEIGHT = 1000;
-	private final int LENGTH = 1000;
-	private final int port = 6789;
+   private JTextField userText;
+   private JTextArea chatWindow;
+   private ObjectOutputStream output;
+   private ObjectInputStream input;
+   private ServerSocket server;
+   private Socket connection;
+   private int HEIGHT;
+   private int LENGTH;
+   private int port;
 
 	//constructor
-	public Server() {
-		super("Instant Messenger");
-		userText = new JTextField();
-		userText.setEditable(false); //before you are connected you cannot type anything in message box 
-		userText.addActionListener(
-			new ActionListener() {
-				public void actionPerformed(ActionEvent event) {
-					sendMessage(event.getActionCommand());
-					userText.setText(""); //turns message box blank after message is sent
+   public Server(int height, int length, int port) {
+      super("Instant Messenger-Server");
+      HEIGHT = height;
+      LENGTH = length;
+      this.port = port;
+      setupMenus();
+      userText = new JTextField();
+      userText.setEditable(false); //before you are connected you cannot type anything in message box 
+      userText.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+               sendMessage(event.getActionCommand());
+               userText.setText(""); //turns message box blank after message is sent
+            
+            }
+         }
+         );
+      add(userText, BorderLayout.NORTH);
+      chatWindow = new JTextArea();
+      add(new JScrollPane(chatWindow));
+      setSize(HEIGHT, LENGTH); //size of window
+      setVisible(true);
+      chatWindow.setFont(new Font("Serif", Font.PLAIN, 30));
+      userText.setFont(new Font("Serif", Font.PLAIN, 30));
+   }
 
-				}
-			}
-		);
-		add(userText, BorderLayout.NORTH);
-		chatWindow = new JTextArea();
-		add(new JScrollPane(chatWindow));
-		setSize(HEIGHT, LENGTH); //size of window
-		setVisible(true);
-		chatWindow.setFont(new Font("Serif", Font.PLAIN, 30));
-		userText.setFont(new Font("Serif", Font.PLAIN, 30));
-	}
+	//sets up the menus for the gui
+   private void setupMenus() {
+      JMenuBar menubar = new JMenuBar();
+   	
+   
+      JMenu file = new JMenu("File");
+   	
+   
+      JMenuItem exit = new JMenuItem("Exit");
+      file.add(exit);
+      menubar.add(file);
+   
+      JMenu help = new JMenu("Help");
+   	
+      JMenuItem about = new JMenuItem("About");
+      help.add(about);
+      menubar.add(help);
+   
+      exit.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+               System.exit(0);
+            }
+         }
+         );
+   
+      about.addActionListener(
+         new ActionListener() {
+            public void actionPerformed(ActionEvent event) {
+               openReadme();
+            }
+         }
+         );
+   
+      setJMenuBar(menubar);
+   
+   	
+   	
+   }	
+
+   //opens readme file
+   private void openReadme() {
+      try {
+         ProcessBuilder pb = new ProcessBuilder("Notepad.exe", "Readme.txt");
+         pb.start();
+      }
+      catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
 
 
 	//set up and run the server 
-	public void startRunning() {
-		try {
-			server = new ServerSocket(port, 100); //port #, backlog(how many people can wait to access this instant messenger)
-			while(true) {
-				try {
-					//connect and have conversation
-					waitForConnection();
-					setupStreams();
-					whileChatting();
-				}
-				catch(EOFException ex) {
-					showMessage("\n Server ended the connection!");
-				}
-				finally {
-					closeStuff();
-				}
-			}
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
+   public void startRunning() {
+      try {
+         server = new ServerSocket(port, 100); //port #, backlog(how many people can wait to access this instant messenger)
+         while(true) {
+            try {
+            	//connect and have conversation
+               waitForConnection();
+               setupStreams();
+               whileChatting();
+            }
+            catch(EOFException ex) {
+               showMessage("\n Server ended the connection!");
+            }
+            finally {
+               closeStuff();
+            }
+         }
+      }
+      catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
 
 	//wait for connection, then once connected give prompt to user
-	private void waitForConnection() throws IOException {
-		showMessage(" Waiting for someone to connect... \n");
-		connection = server.accept(); //once someone asks to connect this accepts the connection to the socket, once connected a connection is created bewteen server and client 
-		showMessage(" Now connected to "+connection.getInetAddress().getHostName());
-
-	}
+   private void waitForConnection() throws IOException {
+      showMessage(" Waiting for someone to connect... \n");
+      connection = server.accept(); //once someone asks to connect this accepts the connection to the socket, once connected a connection is created bewteen server and client 
+      showMessage(" Now connected to "+connection.getInetAddress().getHostName());
+   
+   }
 
 	//get stream to send and receive data
-	private void setupStreams() throws IOException {
-		output = new ObjectOutputStream(connection.getOutputStream()); //creates pathway that allows to connect to another computer
-		output.flush(); // clears the stream so there is no left over data
-		input = new ObjectInputStream(connection.getInputStream()); //sets pathway to receive messages 
-		showMessage("\n Streams are now setup! \n");
-	}
+   private void setupStreams() throws IOException {
+      output = new ObjectOutputStream(connection.getOutputStream()); //creates pathway that allows to connect to another computer
+      output.flush(); // clears the stream so there is no left over data
+      input = new ObjectInputStream(connection.getInputStream()); //sets pathway to receive messages 
+      showMessage("\n Streams are now setup! \n");
+   }
 
 	//during the chat conversation 
-	private void whileChatting() throws IOException {
-		String message = "You are now connected! ";
-		sendMessage(message);
-		ableToType(true);
-		do {
-			//have conversation
-			try {
-				message = (String) input.readObject();
-				//stuff here to unencrypted the message that was received
-				showMessage("\n"+message);
-			}
-			catch(ClassNotFoundException e) {
-				showMessage("\n idk wtf that user sent!");
-			}
-		}
-		while(!message.equals("CLIENT - END")); //if the client wants end then use this string 
-	}
+   private void whileChatting() throws IOException {
+      String message = "You are now connected! ";
+      sendMessage(message);
+      ableToType(true);
+      do {
+      	//have conversation
+         try {
+            message = (String) input.readObject();
+         	//stuff here to unencrypted the message that was received
+            showMessage("\n"+message);
+         }
+         catch(ClassNotFoundException e) {
+            showMessage("\n idk wtf that user sent!");
+         }
+      }
+      while(!message.equals("CLIENT - END")); //if the client wants end then use this string 
+   }
 
 	//close streams and sockets after you are done chating 
-	private void closeStuff() {
-		showMessage("\n Closing connections... \n");
-		ableToType(false);
-		try {
-			output.close();
-			input.close();
-			connection.close();
-		}
-		catch(IOException e) {
-			e.printStackTrace();
-		}
-	}
+   private void closeStuff() {
+      showMessage("\n Closing connections... \n");
+      ableToType(false);
+      try {
+         output.close();
+         input.close();
+         connection.close();
+      }
+      catch(IOException e) {
+         e.printStackTrace();
+      }
+   }
 
 	//send a message to the client 
-	private void sendMessage(String message) {
-		try {
-			//stuff here to encrypt the message before it is sent
-			output.writeObject("SERVER - " + message);
-			output.flush();
-			showMessage("\nSERVER - "  + message);
-		}
-		catch(IOException e) {
-			chatWindow.append("\n ERROR: DUDE I CANT SEND THAT MESSAGE");
-		}
-	}
+   private void sendMessage(String message) {
+      try {
+      	//stuff here to encrypt the message before it is sent
+         output.writeObject("SERVER - " + message);
+         output.flush();
+         showMessage("\nSERVER - "  + message);
+      }
+      catch(IOException e) {
+         chatWindow.append("\n ERROR: DUDE I CANT SEND THAT MESSAGE");
+      }
+   }
 
 	//updates chatWindow
-	private void showMessage(final String text) {
-		//setting aside a thread to update a gui, so we dont have to create an entire new gui when we just want to update part
-		SwingUtilities.invokeLater(
-			new Runnable() {
-				public void run() {
-					chatWindow.append(text); //adds message to the chatWindow
-				}
-			}
-		);
-	}
+   private void showMessage(final String text) {
+   	//setting aside a thread to update a gui, so we dont have to create an entire new gui when we just want to update part
+      SwingUtilities.invokeLater(
+         new Runnable() {
+            public void run() {
+               chatWindow.append(text); //adds message to the chatWindow
+            }
+         }
+         );
+   }
 
 	//lets the user type stuff into there chat box
-	private void ableToType(final boolean trueOrFalse) {
-		SwingUtilities.invokeLater(
-			new Runnable() {
-				public void run() {
-					userText.setEditable(trueOrFalse);
-				}
-			}
-		);
-	} 
+   private void ableToType(final boolean trueOrFalse) {
+      SwingUtilities.invokeLater(
+         new Runnable() {
+            public void run() {
+               userText.setEditable(trueOrFalse);
+            }
+         }
+         );
+   } 
 }
