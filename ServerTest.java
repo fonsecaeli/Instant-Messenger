ae89e636a95b7701786abc9189c03520cc3df77c
Eli <eli.fonseca@live.com>
2016-02-14 01:45:58 -0800

    Update ServerTest.java

--- ServerTest.java
+++ ServerTest.java
@@ -3,7 +3,7 @@ import javax.swing.JFrame;
 
 public class ServerTest {
 	public static void main(String[] args) {
-		Server server = new Server();
+		Server server = new Server(1000, 1000, 6789);
 		server.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		server.startRunning();
 
