c685aff2fff6ecf589c3d25e33e6035716fd2a68
Eli <eli.fonseca@live.com>
2016-02-14 05:38:56 -0800

    Update Login.java

--- Login.java
+++ Login.java
@@ -2,46 +2,103 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.SpringLayout;
 
 
 
 public class Login extends JFrame {
    
-   private JTextField userText;
+   private String[] labels;
+   private JTextField[] textField;
    private JTextArea notifications;
    private JButton submit;
-   private int HEIGHT;
-   private int LENGTH;
+   private int height;
+   private int length;
    private String userName;
+   private String IP;
+   private String bitNumber;
+   private boolean submitted = false;
    
-   public Login(int height, int length) {
+   public boolean forumSubmitted() {
+      return this.submitted;
+   }
+   
+   public String getIP() {
+      return this.IP;
+   }
+   
+   public String getBitNumber() {
+      return this.bitNumber;
+   }
+   
+   public Login(int height, int length, boolean isClient) {
       super("User - Login");
-      userName = "";
-      HEIGHT = height;
-      LENGTH = length;
-      userText = new JTextField();
-      userText.setEditable(true);
-      userText.addActionListener(
+      if(isClient) {
+         labels = new String[3];
+         labels[0] = "UserName: "; 
+         labels[1] = "Key BitLength: "; 
+         labels[2] = "IP Address: ";
+      }
+      else {
+         labels = new String[2];
+         labels[0] = "UserName: ";  
+         labels[1] = "Key BitLength: ";
+      }
+      int labelsLength = labels.length;
+      textField = new JTextField[labelsLength]; 
+      JPanel p = new JPanel(new SpringLayout());
+      for(int i = 0; i < labelsLength; i++) {
+         JLabel l = new JLabel(labels[i], JLabel.TRAILING);
+         p.add(l);
+         textField[i] = new JTextField(10); //sets the default number of spaces each TextField takes up to 10 
+         l.setLabelFor(textField[i]);
+         p.add(textField[i]);
+      }
+      JButton button = new JButton("Submit");
+      p.add(new JLabel());
+      p.add(button);
+    
+    //Lay out the panel.
+      SpringUtilities.makeCompactGrid(p,
+                                    labelsLength + 1, 2, //rows, cols
+                                    7, 7,        //initX, initY
+                                    7, 7);       //xPad, yPad
+   
+   
+      
+      button.addActionListener(
          new ActionListener() {
-            public void actionPerformed(ActionEvent event) {
-               String test = event.getActionCommand();
-               if(test.trim().equals("")) {
-                  showMessage("\nLogin is invalid, please enter something!");   
+         
+            public void actionPerformed(ActionEvent e)
+            {
+               userName = textField[0].getText();
+               bitNumber = textField[1].getText(); //this causes problmes if it is entered as a blakc string 
+                                                   //because it will be processed into an int in the main program
+               if(isClient) {
+                  IP = textField[2].getText();
                }
-               else {
-                  userName = test;
-               }
-               userText.setText("");
+               submitted = true;
+               //TODO
+               //add error correction for input from this form, like if they enter nothing for one of the feilds
             }
-         }
-      );
-      add(userText, BorderLayout.NORTH);
+         });  
+    //Set up the content pane.
+      p.setOpaque(true);  //content panes must be opaque
+      setContentPane(p);
+   
+      this.height = height;
+      this.length = length;
+      
+      
+      
+      
       notifications = new JTextArea();
       add(new JScrollPane(notifications));
-      setSize(HEIGHT, LENGTH); //size of window
+      add(notifications);
+      setSize(height, length); //size of window
       setVisible(true);
       notifications.setFont(new Font("Serif", Font.PLAIN, 30));
-      userText.setFont(new Font("Serif", Font.PLAIN, 30));   
+      //userText.setFont(new Font("Serif", Font.PLAIN, 30));   
    }
    
    public String getUserName() {
@@ -50,7 +107,8 @@ public class Login extends JFrame {
    
    //closes the login window
    public void close() {
-      System.exit(0);
+      setVisible(false);
+      //System.exit(0);
    }
       
    //shows messages on the JFrame
