61bb4f47865d3276e0714d9142ea7175d2ee2eea
Eli <eli.fonseca@live.com>
2016-02-14 05:35:53 -0800

    Update ClientTest.java

--- ClientTest.java
+++ ClientTest.java
@@ -1,10 +1,36 @@
 import javax.swing.JFrame;
 
 public class ClientTest {
-	public static void main(String[] args) {
+
+   private static String userName = "";
+   private static String IP = "";
+   private static int bitLength = 0;
+
+public static void main(String[] args) {
+      Login log = new Login(300, 300, true);
+      getUserInfo(log);
 		Client client; 
-		client = new Client("127.0.0.1", 1000, 1000, 6789); //means local host, so will connect to this computer
+		client = new Client(IP, 1000, 1000, 6789, userName, bitLength); //means local host, so will connect to this computer
 		client.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		client.startRunning();
 	}
+   
+   private static void getUserInfo(Login log) throws InterruptedException {
+   do {
+      if(log.forumSubmitted()) {
+         if(!log.getUserName().equals("") && !log.getIP().equals("") && !log.getBitNumber().equals("")) {
+            userName = log.getUserName();
+            IP = log.getIP();
+            bitLength = Integer.valueOf(log.getBitNumber()); 
+            Thread.sleep(1000);
+            log.close();
+            break;
+            //log.close();
+         }
+      }
+      Thread.sleep(100);
+      }
+      while(true);
+   }
+
 }
